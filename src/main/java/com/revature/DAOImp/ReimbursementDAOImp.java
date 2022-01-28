4cc4135d3f0349907845b4e1350d5e287421f05e
MatthewIngraham <mingra06@baker.edu>
2022-01-28 11:56:52 -0500

    Another method

--- src/main/java/com/revature/DAOImp/ReimbursementDAOImp.java
+++ src/main/java/com/revature/DAOImp/ReimbursementDAOImp.java
@@ -6,10 +6,8 @@ import com.revature.Models.Status;
 import com.revature.Models.User;
 import com.revature.utils.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,7 +43,31 @@ public class ReimbursementDAOImp implements ReimbursementDAO {
 
     @Override
     public List<Reimbursement> getReimbursementsByAuthor(int authorUserId) {
-        return null;
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "SELECT * FROM ers_reimbursement WHERE reimb_author = " + authorUserId + ";";
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            List<Reimbursement> list = new ArrayList<Reimbursement>();
+            Reimbursement reimbursement = new Reimbursement();
+
+            while (result.next()){
+                reimbursement.setId(result.getInt("reimb_id"));
+                reimbursement.setAmount(result.getDouble("reimb_amount"));
+                reimbursement.setTimeSubmitted(result.getTimestamp("reimb_submitted"));
+                reimbursement.setTimeResolved(result.getTimestamp("reimb_resolved"));
+                reimbursement.setDescription(result.getString("reimb_description"));
+                reimbursement.setReceipt(result.getBytes("reimb_receipt"));
+                reimbursement.setAuthorUserId(result.getInt("reimb_author"));
+                reimbursement.setResolverUserId(result.getInt("reimb_resolver"));
+                reimbursement.setStatusId(result.getInt("reimb_status_id"));
+                reimbursement.setTypeId(result.getInt("reimb_type_id"));
+            }
+            return list;
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
+        return new ArrayList<Reimbursement>();
     }
 
     @Override
