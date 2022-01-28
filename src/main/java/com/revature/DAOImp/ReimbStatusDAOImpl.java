6ae619cdc2b50eb77ec192bff04faf87987cee05
rogerjarad <rogerjaradgaulke@gmail.com>
2022-01-28 14:08:57 -0800

    statusDAOImpl

--- src/main/java/com/revature/DAOImp/ReimbStatusDAOImpl.java
+++ src/main/java/com/revature/DAOImp/ReimbStatusDAOImpl.java
@@ -1,9 +1,15 @@
+//Not tested
 package com.revature.DAOImp;
 
 import com.revature.DAO.ReimbStatusDAO;
 import com.revature.Models.ReimbStatus;
+import com.revature.Models.ReimbType;
+import com.revature.utils.ConnectionUtil;
 import com.revature.utils.Status;
+import com.revature.utils.Type;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReimbStatusDAOImpl implements ReimbStatusDAO {
@@ -11,21 +17,84 @@ public class ReimbStatusDAOImpl implements ReimbStatusDAO {
 
     @Override
     public List<ReimbStatus> getAllStatus() {
-        return null;
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "SELECT * FROM ers_reimbursement_status;";
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            List<ReimbStatus> list = new ArrayList<ReimbStatus>();
+            ReimbStatus status = new ReimbStatus();
+
+
+
+            while (result.next()){
+                status.setReimbStatusID(result.getInt("reimb_status_id"));
+                status.setStatus(Status.values()[result.getInt("reimb_status")]);
+                list.add(status);
+            }
+            return list;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return new ArrayList<ReimbStatus>();
     }
 
     @Override
     public ReimbStatus getStatisByID(int id) {
-        return null;
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "SELECT * FROM ers_reimbursement_status WHERE reimb_status_id = + " + id + ";";
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            List<ReimbStatus> list = new ArrayList<ReimbStatus>();
+            ReimbStatus status = new ReimbStatus();
+
+
+
+            while (result.next()){
+                status.setReimbStatusID(result.getInt("reimb_status_id"));
+                status.setStatus(Status.values()[result.getInt("reimb_status")]);
+                list.add(status);
+            }
+            return status;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return new ReimbStatus();
     }
 
     @Override
     public boolean addStatus(ReimbStatus newStatus) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "INSERT INTO ers_reimbursement_status (reimb_status_id, reimb_status)" +
+                    "VALUES (?, ?);";
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setInt(1, newStatus.getReimbStatusID());
+            statement.setInt(2, newStatus.getStatus().ordinal());
+
+            statement.execute();
+            return true;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public boolean deleteStatus(int id) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "DELETE FROM ers_reimbursement_status WHERE reimb_status_id = ?;";
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setInt(1, id);
+            statement.execute();
+
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+
+        }
         return false;
     }
 }
