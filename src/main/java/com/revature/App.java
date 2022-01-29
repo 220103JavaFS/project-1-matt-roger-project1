7e73c9840795b03cca8cb27f447a8cfdabfb7675
MatthewIngraham <mingra06@baker.edu>
2022-01-29 14:39:01 -0500

    Added Access Manager and App

---
+++ src/main/java/com/revature/App.java
@@ -0,0 +1,28 @@
+package com.revature;
+
+import com.revature.Controller.Controller;
+import com.revature.Controller.UserController;
+import com.revature.Models.AccessManager;
+import io.javalin.Javalin;
+
+
+public class App {
+    private static Javalin app;
+
+    public static void main(String[] args) {
+
+        app = Javalin.create(config -> {
+            config.accessManager(new AccessManager());}); //This represents the configuration of the framework at runtime.
+        configure(new UserController());
+
+        app.start();
+    }
+
+    public static void configure(Controller... controllers){
+        for(Controller c: controllers){
+            c.addRoutes(app);
+        }
+    }
+
+
+}
