3f666a42587b884910a0bc73ab7cfd7235969846
rogerjarad <rogerjaradgaulke@gmail.com>
2022-02-02 15:00:05 -0800

    working on approve

--- src/main/resources/staticFiles/approvereimbursement.js
+++ src/main/resources/staticFiles/approvereimbursement.js
@@ -1,6 +1,18 @@
 let getAllbtn = document.getElementById("getAllbtn");
+let reimbursementTable = document.getElementById("reimbursementTbl");
+
+const url = "http://localhost:8080/reimbursements"
 
 getAllbtn.addEventListener("click", getAllReimbursements);
+
+
+document.querySelectorAll('.status-btn').forEach(button => {
+  button.addEventListener("click", e => {
+    e.target.closest('tr').querySelector('td.status').innerText = e.target.dataset.status;
+  });
+});
+
+
 
 
 async function getAllReimbursements(){
@@ -10,7 +22,7 @@ async function getAllReimbursements(){
   
     if(response.status===200){
       let reimbursements = await response.json();
-      populatereimbursment(reimbursement);
+      populatereimbursment(reimbursements);
     } else{
       console.log("There was an error getting your reimbursements.")
     }
