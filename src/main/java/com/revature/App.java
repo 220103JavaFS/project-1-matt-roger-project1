c530f718ed32b8b9feec830006c01435e60d2754
MatthewIngraham <mingra06@baker.edu>
2022-02-02 09:40:12 -0500

    static files hosted

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -12,6 +12,10 @@ public class App {
     public static void main(String[] args) {
 
         app = Javalin.create(config -> {
+            config.addStaticFiles(staticFiles -> {
+                staticFiles.hostedPath = "/resources/static";
+            });
+
             config.accessManager(new AccessManager());}); //This represents the configuration of the framework at runtime.
         configure(new UserController(), new ReimbursementController(), new LoginController(), new ReimbStatusController(), new ReimbTypeController());
 
